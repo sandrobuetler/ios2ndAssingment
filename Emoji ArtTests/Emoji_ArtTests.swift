9426259deb4ded6ca7f376987778f3496b7a37a8
Sandro Bütler <sandro.buetler@students.fhnw.ch>
2021-02-16 09:02:45 +0100

    added test template in xcode

---
+++ Emoji ArtTests/Emoji_ArtTests.swift
@@ -0,0 +1,33 @@
+//
+//  Emoji_ArtTests.swift
+//  Emoji ArtTests
+//
+//  Created by Sandro Bütler on 16.02.21.
+//  Copyright © 2021 fhnw. All rights reserved.
+//
+
+import XCTest
+
+class Emoji_ArtTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
