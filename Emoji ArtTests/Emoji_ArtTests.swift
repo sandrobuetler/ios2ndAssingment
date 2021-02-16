e5ca7d085264a3999221da602b5f271a88949365
Sandro Bütler <sandro.buetler@students.fhnw.ch>
2021-02-16 09:52:00 +0100

    added unit test (all failing)

--- Emoji ArtTests/Emoji_ArtTests.swift
+++ Emoji ArtTests/Emoji_ArtTests.swift
@@ -7,18 +7,77 @@
 //
 
 import XCTest
+//quelle: https://www.youtube.com/watch?v=-eiY9eauJr0&ab_channel=iOSAcademy
+@testable import Emoji_Art
 
 class Emoji_ArtTests: XCTestCase {
 
+    private var emojiArt = EmojiArt()
+    
     override func setUpWithError() throws {
+        //quelle: https://www.swiftbysundell.com/basics/unit-testing/
+        super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        emojiArt = EmojiArt()
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
+    func testAddEmojiTextEmpty(){
+        //given
+        //var emojiArt = EmojiArt()
+        let givenEmojiCount = emojiArt.emojis.count
+        
+        //when
+        emojiArt.addEmoji("", x: 50, y: 50, size: 1)
+        
+        //then
+        let emojiCount = emojiArt.emojis.count
+        XCTAssertEqual(emojiCount, givenEmojiCount)
+    }
+    
+    func testAddEmojiSizeGreater(){
+        //given
+        let givenEmojiCount = emojiArt.emojis.count
+        
+        //when
+        emojiArt.addEmoji("🙈", x: 0, y: 0, size: 2)
+        
+        //then
+        let emojiCount = emojiArt.emojis.count
+        XCTAssertEqual(emojiCount, givenEmojiCount)
+    }
+    
+    func testAddEmojiIdIncrements(){
+        //given
+        
+        //when
+        emojiArt.addEmoji("🙈", x: 0, y: 0, size: 1)
+        emojiArt.addEmoji("🙈", x: 0, y: 0, size: 1)
+        
+        //then
+        XCTAssertEqual(emojiArt.emojis[1].id, emojiArt.emojis[0].id + 1)
+        XCTAssertEqual(emojiArt.emojis.count, emojiArt.emojis[emojiArt.emojis.count - 1].id)
+        
+        //when
+        emojiArt.addEmoji("", x: 0, y: 0, size: 1)
+        emojiArt.addEmoji("🙈", x: 0, y: 0, size: 2)
+        
+        //then
+        XCTAssertEqual(emojiArt.emojis.count, 2)
+    }
+    
     func testExample() throws {
+        
+        //given
+        
+        // when
+        
+        //then
+        
+ 
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
